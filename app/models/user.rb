7507bb3151d62d042ce75c729d75ecfab136cf99
Patryk Ławicki <patlaw96@gmail.com>
2014-03-20 19:47:03 +0100

    pfhu

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable, :rememberable, :recoverable,
+  # :confirmable, :lockable, :timeoutable and :omniauthable, :rememberable, 
   devise :database_authenticatable, :registerable,
           :trackable, :validatable
 end
