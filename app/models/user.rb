9138bd14f39ef63442fff4bf4c52abfa03bb039b
Patryk Ławicki <patlaw96@gmail.com>
2014-03-20 19:25:16 +0100

    problems

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable, :rememberable, :recoverable,
   devise :database_authenticatable, :registerable,
-          :trackable, :validatable
+          :trackable, :validatable, :recoverable
 end
