4972f6ae3390394b75ce26cb39db4e38375c1fbd
Patryk Ławicki <patlaw96@gmail.com>
2014-03-20 19:22:43 +0100

    4

--- db/schema.rb
+++ db/schema.rb
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 20140319184854) do
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    #t.string   "reset_password_token"
-    #t.datetime "reset_password_sent_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
@@ -36,6 +36,6 @@ ActiveRecord::Schema.define(version: 20140319184854) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  #add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
