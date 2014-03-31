cc0ba8c228917e5f04a4865e4d189003eef45af7
true-or-false <patlaw96@gmail.com>
2014-03-31 17:26:18 +0200

    search with infinitescroll

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -25,10 +25,16 @@ class BeansController < ApplicationController
   def edit
   end
 
+  # GET /gists/show
+  def search
+     @beans = Bean.search(params[:lang],params[:desc])
+     @beans = Kaminari.paginate_array(@beans).page(params[:page])
+  end
+
   # POST /beans
   # POST /beans.json
   def create
-    @bean = Bean.new(bean_params)
+    @bean = Bean.new(change_lang(bean_params))
 
     respond_to do |format|
       if @bean.save
@@ -45,7 +51,7 @@ class BeansController < ApplicationController
   # PATCH/PUT /beans/1.json
   def update
     respond_to do |format|
-      if @bean.update(bean_params)
+      if @bean.update(change_lang(bean_params))
         format.html { redirect_to @bean, notice: 'Bean was successfully updated.' }
         format.json { head :no_content }
       else
@@ -74,5 +80,35 @@ class BeansController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def bean_params
       params.require(:bean).permit(:snippet, :lang, :description)
+    end
+
+    #Pygments validating lang
+    def not_allowed?(lang)
+      allowed = Pygments.lexers.flatten.select.with_index{|a,i| (i+1) % 2 == 0}
+      allowed = allowed.map{|a| a[:aliases][0]}
+      allowed = allowed.delete_if{|a| (/-cpp/ =~ a || /-objc/ =~ a)}
+      allowed = allowed.sort
+
+      good = false
+      allowed.each do |l|
+
+        if l == lang
+          good = true
+          break
+        end
+
+      end
+
+
+      !good_lang
+    end
+
+    def change_lang(gists)
+     
+      if not_allowed?(gists[:lang])
+        gists[:lang] = "text"
+      end
+
+      return gists
     end
 end
--- app/models/bean.rb
+++ app/models/bean.rb
@@ -1,4 +1,10 @@
 class Bean < ActiveRecord::Base
 	attr_default :lang, "ruby"
-
+	def self.search(search, search2)
+  		if search && search2
+       		find(:all, :conditions => ['lang=? AND description LIKE ?', "#{search}","%#{search2}%"])
+    	else
+     		limit(0)
+    	end
+   	end
 end
